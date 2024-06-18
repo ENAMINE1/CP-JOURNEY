5278bcbdb304bc99f2db6dcff0ad7fdf622864cd
ENAMINE1 <kshashwat.iit@gmail.com>
2024-06-18 20:16:32 +0530

    Automatic commit 2024-06-18 20:16:32

---
+++ QUESTIONS/INTERESTING PROBLEMS/C - Multiplication 3 .cpp
@@ -0,0 +1,110 @@
+// Date: 18-06-2024
+// Start Time: 19:44:31
+// End Time  : 20:02:33
+// Time Taken: 18 minutes
+// Author: Shashwat Kumar
+// QUESTION LINK: https://atcoder.jp/contests/abc169/tasks/abc169_c
+// Rating: Medium
+// Description: How to take product of a large number with a decimal number
+// Solved: No
+// Learning: you can take the input of decimal number in terms of its integer part and decimal part if
+
+/****************************************************Pre Processor***************************************************/
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+#ifdef LOCAL
+#include "./lib/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+/****************************************************Definition***************************************************/
+#define endl '\n'
+#define pb push_back
+#define rep(i, n) for (ll i = 0; i < n; i++)
+#define repi(i, a, n) for (ll i = a; i < n; i++)
+#define repii(i, a, n, b) for (ll i = a; i < n; i += b)
+// #define int long long
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vpii;
+
+/**************************************************Global Variables*************************************************/
+const ll MAXM = 1e5;
+const int mod = 1e9 + 7;
+const int INF = 1e9 + 5;
+const long long INFF = 1000000000000000005LL;
+const double EPS = 1e-9;
+const double PI = acos(-1);
+vector<long> factors[MAXM + 5];
+vpii dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+/**************************************************Utility Functions***********************************************/
+void init()
+{
+    for (ll i = 1; i <= MAXM; i++)
+    {
+        for (ll j = i; j <= MAXM; j += i)
+        {
+            factors[j].pb(i);
+        }
+    }
+}
+
+int bin_pow(int base, int pow)
+{
+    int ans = 1;
+    while (pow)
+    {
+        if (pow & 1)
+        {
+            ans = 1LL * ans * base % mod;
+        }
+        base = 1LL * base * base % mod;
+        pow /= 2;
+    }
+    return ans;
+}
+/***************************************************Main Function**************************************************/
+
+void solve()
+{
+    ll a, b;
+    cin >> a >> b;
+    char c;
+    cin >> c;
+    int decimal;
+    cin >> decimal;
+    b *= 100;
+    b += decimal;
+    ll ans = a * b;
+
+    cout << ans / 100;
+}
+
+signed main()
+{
+    auto begin = std::chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    std::cout.tie(NULL);
+
+    int t = 1;
+    // cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+#ifdef LOCAL
+        std::cerr << "Case # " << i << endl;
+        std::cout << "Case #" << i << endl;
+#endif
+        solve();
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+    return 0;
+}
